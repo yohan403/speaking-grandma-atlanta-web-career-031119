0e34203ba2cb9526e7eed7569b56e94b2b3f09ca
John Cho <johnycho16@gmail.com>
2019-02-25 01:52:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@
 def speak_to_grandma(string)
   if string == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif string !== string.uppercase
+  elsif string != string.uppercase
     return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
