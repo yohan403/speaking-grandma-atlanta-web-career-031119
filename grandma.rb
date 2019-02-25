1b5cacccbbe1798eec1ab26ab5b6c1ce8123d124
John Cho <johnycho16@gmail.com>
2019-02-25 01:52:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,17 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+def speak_to_grandma(string)
+  if string == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+  elsif string !== string.uppercase
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
+
+speak_to_grandma("john")
+speak_to_grandma("I LOVE YOU PUMPKIN")
+speak_tp_grandma("HELLO!")
